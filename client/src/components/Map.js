4f6aec5c7237da409047ccc685bcc18f51e1bf0d
hsimao <e087754958@gmail.com>
2020-01-28 20:54:09 +0800

    add: 新增當前用戶坐標 Marker

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
-import ReactMapGL, { NavigationControl } from "react-map-gl";
+import React, { useState, useEffect } from "react";
+import ReactMapGL, { NavigationControl, Marker } from "react-map-gl";
 import { withStyles } from "@material-ui/core/styles";
+import PinIcon from "./PinIcon";
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
@@ -13,6 +14,23 @@ const INITIAL_VIEWPORT = {
 
 const Map = ({ classes }) => {
   const [viewport, setViewport] = useState(INITIAL_VIEWPORT);
+  const [userPosition, setUserPosition] = useState(null);
+
+  // useEffect 會在組件 render 完後執行, 以及每次更新之後執行。
+  useEffect(() => {
+    getUserPosition();
+  });
+
+  const getUserPosition = () => {
+    // 如果瀏覽器有支援 geolocation api, 就使用取得用戶當前座標
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(position => {
+        const { latitude, longitude } = position.coords;
+        setViewport({ ...viewport, latitude, longitude });
+        setUserPosition({ latitude, longitude });
+      });
+    }
+  };
 
   return (
     <div className={classes.root}>
@@ -30,6 +48,18 @@ const Map = ({ classes }) => {
             onViewportChange={newViewport => setViewport(newViewport)}
           />
         </div>
+
+        {/* Pin for User's Current Position */}
+        {userPosition && (
+          <Marker
+            latitude={userPosition.latitude}
+            longitude={userPosition.longitude}
+            offsetLeft={-19}
+            offsetTop={-37}
+          >
+            <PinIcon size="40" color="#2196f3" />
+          </Marker>
+        )}
       </ReactMapGL>
     </div>
   );
--- client/src/index.js
+++ client/src/index.js
@@ -14,8 +14,7 @@ import * as serviceWorker from "./serviceWorker";
 const Root = () => {
   const initialState = useContext(Context);
   const [state, dispatch] = useReducer(reducer, initialState);
-  console.log("Root");
-  console.log({ state });
+
   return (
     <Router>
       <Context.Provider value={{ state, dispatch }}>
