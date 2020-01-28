738b09dfdecb7f560bd2de0082a6d507262e9243
hsimao <e087754958@gmail.com>
2020-01-28 13:56:53 +0800

    add: setting mongoDB

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
 const { ApolloServer } = require('apollo-server')
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connected!'))
+  .catch(err => console.error(err))
 
 const typeDefs = require('./typeDefs')
 const resolvers = require('./resolvers')
