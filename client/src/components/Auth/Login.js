93e022fcfae0a8a588249962bcab7d8a3768a76f
hsimao <e087754958@gmail.com>
2020-01-28 15:18:40 +0800

    add: google OAuth setting

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { GoogleLogin } from "react-google-login";
 import { withStyles } from "@material-ui/core/styles";
 // import Typography from "@material-ui/core/Typography";
 
 const Login = ({ classes }) => {
-  return <div>Login</div>;
+  const onSuccess = googleUser => {
+    const idToken = googleUser.getAuthResponse().id_token;
+    console.log(idToken);
+  };
+  return (
+    <GoogleLogin
+      clientId="596539049831-lguinmmg7hntglc8anb95ft9si5aepbo.apps.googleusercontent.com"
+      onSuccess={onSuccess}
+      isSignedIn={true}
+    />
+  );
 };
 
 const styles = {
