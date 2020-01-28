c86308a802f7c2b251d0056d3e328f17580fac17
hsimao <e087754958@gmail.com>
2020-01-28 18:06:31 +0800

    add: logout logic, btn

--- client/src/components/Auth/Signout.js
+++ client/src/components/Auth/Signout.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GoogleLogout } from "react-google-login";
 import { withStyles } from "@material-ui/core/styles";
-// import ExitToApp from "@material-ui/icons/ExitToApp";
-// import Typography from "@material-ui/core/Typography";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import Typography from "@material-ui/core/Typography";
+
+import Context from "../../context";
 
 const Signout = ({ classes }) => {
-  return <div>Signout</div>;
+  const { dispatch } = useContext(Context);
+
+  const onSignout = () => {
+    dispatch({ type: "SIGNOUT_USER" });
+  };
+
+  return (
+    <GoogleLogout
+      onLogoutSuccess={onSignout}
+      buttonText="Signout"
+      render={({ onClick }) => (
+        <span className={classes.root} onClick={onClick}>
+          <Typography
+            variant="body1"
+            color="inherit"
+            className={classes.buttonText}
+          >
+            Signout
+          </Typography>
+          <ExitToAppIcon className={classes.buttonIcon}></ExitToAppIcon>
+        </span>
+      )}
+    />
+  );
 };
 
 const styles = {
@@ -12,12 +38,8 @@ const styles = {
     cursor: "pointer",
     display: "flex"
   },
-  buttonText: {
-    color: "orange"
-  },
   buttonIcon: {
-    marginLeft: "5px",
-    color: "orange"
+    marginLeft: "5px"
   }
 };
 
