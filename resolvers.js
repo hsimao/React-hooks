425f0d25520a5ad698421a21334ccc983d323577
hsimao <e087754958@gmail.com>
2020-01-29 22:17:56 +0800

    add: createPin logic

--- resolvers.js
+++ resolvers.js
@@ -1,4 +1,5 @@
 const { AuthenticationError } = require("apollo-server");
+const Pin = require("./models/Pin");
 
 // 封裝高階函式 authenticated 每次使用 graphql 查詢用戶時, 先判斷是否有 currentUser (在 server.js 內 new ApolloServer 時調用 controllers 內的 findOrCreateUser 邏輯判斷)
 const authenticated = next => (root, args, ctx, info) => {
@@ -8,8 +9,22 @@ const authenticated = next => (root, args, ctx, info) => {
   return next(root, args, ctx, info);
 };
 
+// 新增標籤, 儲存到資料庫後填充 author 資訊後回傳
+const createPin = authenticated(async (root, args, ctx) => {
+  const newPin = await new Pin({
+    ...args.input,
+    author: ctx.currentUser._id
+  }).save();
+
+  const pinAdded = await Pin.populate(newPin, "author");
+  return pinAdded;
+});
+
 module.exports = {
   Query: {
     me: authenticated((root, args, ctx) => ctx.currentUser)
+  },
+  Mutation: {
+    createPin: createPin
   }
 };
