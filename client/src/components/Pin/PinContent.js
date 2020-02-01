8e523a5d67cf3a5368dcf25e483a329a16641a9f
hsimao <e087754958@gmail.com>
2020-02-01 16:54:38 +0800

    add: 選中樣式內容, 留言樣式

--- client/src/components/Pin/PinContent.js
+++ client/src/components/Pin/PinContent.js
@@ -5,13 +5,15 @@ import AccessTimeIcon from "@material-ui/icons/AccessTime";
 import FaceIcon from "@material-ui/icons/Face";
 import Context from "../../context";
 import format from "date-fns/format";
+import CreateComment from "../Comment/CreateComment";
+import Comments from "../Comment/Comments";
 
 const PinContent = ({ classes }) => {
   const { state } = useContext(Context);
   const { title, content, author, createdAt, comments } = state.currentPin;
   return (
     <div className={classes.root}>
-      <Typography component="h2" variant="h4" color="primary" butterBottom>
+      <Typography component="h2" variant="h4" color="primary" gutterBottom>
         {title}
       </Typography>
       <Typography
@@ -19,7 +21,7 @@ const PinContent = ({ classes }) => {
         component="h3"
         variant="h6"
         color="inherit"
-        butterBottom
+        gutterBottom
       >
         <FaceIcon className={classes.icon} />
         {author.name}
@@ -36,6 +38,10 @@ const PinContent = ({ classes }) => {
       <Typography variant="subtitle1" gutterBottom className={classes.content}>
         {content}
       </Typography>
+
+      {/* 留言區塊 */}
+      <CreateComment />
+      <Comments comments={comments} />
     </div>
   );
 };
@@ -44,7 +50,7 @@ const styles = theme => ({
   root: {
     padding: "1em 0.5em",
     textAlign: "center",
-    width: "100%"
+    width: "80%"
   },
   icon: {
     marginLeft: theme.spacing.unit,
@@ -57,7 +63,6 @@ const styles = theme => ({
   },
   content: {
     textAlign: "left",
-    maxWidth: "80%",
     borderTop: "solid 1px gray",
     margin: "16px auto 0 auto",
     padding: "16px 0"
